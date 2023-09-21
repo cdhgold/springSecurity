37072f312fb1bd8078af4eac8848233f0082a929
cdhgold <cdhgold@naver.com>
2023-09-21 22:49:36 +0900

    userController

--- src/main/java/com/mang/example/security/app/user/controller/UserController.java
+++ src/main/java/com/mang/example/security/app/user/controller/UserController.java
@@ -3,12 +3,14 @@ package com.mang.example.security.app.user.controller;
 import com.mang.example.security.app.user.model.UserVO;
 import com.mang.example.security.app.user.service.UserService;
 import com.mang.example.security.enums.role.UserRole;
+ 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +25,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Log4j2
 public class UserController {
 
-    private final UserService userService;
+    @Autowired 
+    private  UserService userService; 
 
     @NonNull
     private BCryptPasswordEncoder passwordEncoder;
@@ -37,7 +40,7 @@ public class UserController {
     @PostMapping(value = "/login")
     public String login(HttpServletRequest request, RedirectAttributes redirectAttributes, @ModelAttribute UserVO userVO){
         log.error("@@@");
-        String userPw = userVO.getUserPw();
+        String userPw = userVO.getUserPw(); 
         userVO = userService.findUserByUserEmail(userVO.getUserEmail());
         if(userVO == null || !passwordEncoder.matches(userPw, userVO.getUserPw())){
             redirectAttributes.addFlashAttribute("rsMsg", "아이디 또는 비밀번호가 잘못되었습니다.");
@@ -57,11 +60,11 @@ public class UserController {
         if(userService.createUser(userVO) == null){
             log.error("Create Admin Error");
         }
-
-        userVO.setUserEmail("admin@naver.com");
-        userVO.setUserPw(passwordEncoder.encode("test"));
-        userVO.setRole(UserRole.ADMIN);
-        if(userService.createUser(userVO) == null){
+        UserVO user2 = new UserVO();
+        user2.setUserEmail("admin@naver.com");
+        user2.setUserPw(passwordEncoder.encode("test"));
+        user2.setRole(UserRole.ADMIN);
+        if(userService.createUser(user2) == null){
             log.error("Create Admin Error");
         }
         return "redirect:/index";
